b66a3a9854eaf26558fe78d2e00c9e0bcf6b649a
Samir Caus <scaus@adobe.com>
2025-01-08 13:23:48 +0100

    update globalsettings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,11 +12,16 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <>
-      <Script id="targetGlobalSettings" strategy="beforeInteractive">
-        {`window.targetGlobalSettings = {
-        cookieDomain: "free-nextjs-admin-dashboard-h6echej2s-samircaus-projects.vercel.app/"
-      }`}
-      </Script>
+      <script id="targetGlobalSettings"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.targetGlobalSettings = {
+                cookieDomain: "free-nextjs-admin-dashboard-h6echej2s-samircaus-projects.vercel.app"
+              }
+            `,
+          }}
+        />
+
       {/* <Script src="https://assets.adobedtm.com/6a203c8a0ff8/cc2e28cff2ef/launch-7be2e2d66423-development.js" strategy="beforeInteractive"/> */}
       <Script id="loaded">
         {`console.log("loaded")`}
