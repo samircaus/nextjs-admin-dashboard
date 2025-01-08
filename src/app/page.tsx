6a19184defbc329a86e607feecd1516d377190c0
Samir Caus <scaus@adobe.com>
2025-01-08 11:21:40 +0100

    add script

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import ECommerce from "@/components/Dashboard/E-commerce";
 import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
-
+import Script from 'next/script'
+ 
 export const metadata: Metadata = {
   title:
     "Next.js E-commerce Dashboard | TailAdmin - Next.js Dashboard Template",
@@ -11,6 +12,7 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <>
+    <Script src="https://assets.adobedtm.com/6a203c8a0ff8/cc2e28cff2ef/launch-7be2e2d66423-development.min.js" strategy="beforeInteractive" />
       <DefaultLayout>
         <ECommerce />
       </DefaultLayout>
