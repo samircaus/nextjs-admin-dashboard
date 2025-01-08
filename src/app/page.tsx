cca70f9cfffe550365f72b51d9392e81cb6bb44e
Samir Caus <scaus@adobe.com>
2025-01-08 13:14:12 +0100

    test script loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import ECommerce from "@/components/Dashboard/E-commerce";
 import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import Script from 'next/script'
- 
+
 export const metadata: Metadata = {
   title:
     "Next.js E-commerce Dashboard | TailAdmin - Next.js Dashboard Template",
@@ -12,12 +12,16 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <>
-    <Script >
-      {`window.targetGlobalSettings = {
+      <Script id="targetGlobalSettings" strategy="beforeInteractive">
+        {`window.targetGlobalSettings = {
         cookieDomain: "free-nextjs-admin-dashboard-h6echej2s-samircaus-projects.vercel.app/"
       }`}
-    </Script>
-    <Script src="https://assets.adobedtm.com/6a203c8a0ff8/cc2e28cff2ef/launch-7be2e2d66423-development.js" strategy="afterInteractive" />
+      </Script>
+      {/* <Script src="https://assets.adobedtm.com/6a203c8a0ff8/cc2e28cff2ef/launch-7be2e2d66423-development.js" strategy="beforeInteractive"/> */}
+      <Script id="loaded">
+        {`console.log("loaded")`}
+      </Script>
+      <script id="dtm" async src="https://assets.adobedtm.com/6a203c8a0ff8/cc2e28cff2ef/launch-7be2e2d66423-development.js"/>
       <DefaultLayout>
         <ECommerce />
       </DefaultLayout>
