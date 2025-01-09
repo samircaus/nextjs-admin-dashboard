c1fde3ab3d0b02af14027adee9497319e13a8ce2
Samir Caus <scaus@adobe.com>
2025-01-09 09:38:40 +0100

    add at property

--- public/config.js
+++ public/config.js
@@ -2,3 +2,8 @@ window.targetGlobalSettings = {
     cookieDomain: "free-nextjs-admin-dashboard-samircaus-projects.vercel.app"
 }
 
+function targetPageParams() {
+    return {
+      "at_property": "e4e2c7e4-2db0-fd26-fd1f-cb220b91e03f"
+    };
+}
